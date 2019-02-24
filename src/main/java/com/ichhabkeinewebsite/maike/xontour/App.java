783024e8b9117464405e813fa56bf21cc37d076f
Maike Orlikowski <maike.orlikowski@campus.tu-berlin.de>
2019-02-24 15:45:30 +0100

    first commit

---
+++ src/main/java/com/ichhabkeinewebsite/maike/xontour/App.java
@@ -0,0 +1,13 @@
+package com.ichhabkeinewebsite.maike.xontour;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
