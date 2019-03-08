9b5dd09834142bb7cd0b73b0da6cef3f85253f8f
Jan-Hendrik Peters <jan-hendrik.peters@idealo.de>
2019-03-08 10:09:30 +0100

    add a failing test for a bug regarding umlauts add a question in a FIXME block for mmaike

--- src/test/java/maike/whoisontour/tourcheck/TicketMasterClientTest.java
+++ src/test/java/maike/whoisontour/tourcheck/TicketMasterClientTest.java
@@ -10,10 +10,15 @@ import org.junit.Test;
 
 public class TicketMasterClientTest {
 
+	private ApiKeyProvider apiKeyProvider = mock(ApiKeyProvider.class);
+
+	// FIXME I find it hard to find the differences between the parameterized buildApiRequest() invocations
+	// how about removing the logic from that method and instead make it two methods:
+	// one to find the id and one for the querying of upcoming events?
+
 	@Test
-	public void buildApiRequest() {
+	public void buildsUrl() {
 		// given
-		ApiKeyProvider apiKeyProvider = mock(ApiKeyProvider.class);
 		when(apiKeyProvider.getApiKey()).thenReturn("foo");
 
 		TicketMasterClient ticketMasterClient = new TicketMasterClient(apiKeyProvider);
@@ -24,4 +29,36 @@ public class TicketMasterClientTest {
 		// then
 		assertThat(result.toString()).isEqualTo("https://app.ticketmaster.com/discovery/v2/type?apikey=foo&attractionId=666&countryCode=DE");
 	}
+
+	@Test
+	public void buildsUrlForAttractionsCorrectly() {
+		// given
+		when(apiKeyProvider.getApiKey()).thenReturn("foo");
+
+		TicketMasterClient ticketMasterClient = new TicketMasterClient(apiKeyProvider);
+
+		// when
+		URL result = ticketMasterClient.buildApiRequest("attractions", "666", "slayer");
+
+		// then
+		assertThat(result.toString()).isEqualTo("https://app.ticketmaster.com/discovery/v2/attractions?apikey=foo&attractionId=666&countryCode=DE&keyword=slayer");
+	}
+
+	// TODO mmmaike
+	@Test
+	public void encodesGermanUmalautsCorrectly() {
+		// given
+		when(apiKeyProvider.getApiKey()).thenReturn("foo");
+
+		TicketMasterClient ticketMasterClient = new TicketMasterClient(apiKeyProvider);
+
+		// when
+		URL result = ticketMasterClient.buildApiRequest("attractions", "666", "släyer");
+
+		// then
+		assertThat(result.toString()).isEqualTo("https://app.ticketmaster.com/discovery/v2/attractions?apikey=foo&attractionId=666&countryCode=DE&keyword=sl%C3%A4yer");
+	}
+
+
+
 }
